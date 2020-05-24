c5f21d820cddcae812b53438dd43a6a4c39af236
Bappy0X <47988616+Bappy0X@users.noreply.github.com>
2020-05-24 02:03:15 +0100

    Notifications and Tray Icon

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const { app, BrowserWindow } = require("electron")
+const { app, BrowserWindow, Tray, Menu } = require("electron")
 const path = require("path")
 const url = require("url")
 
-function createWindow(){
+function createWindow() {
   const window = new BrowserWindow({
     width: 1280,
     height: 720,
@@ -25,13 +25,37 @@ function createWindow(){
 app.whenReady().then(() => {
   createWindow()
   
+  //Create TrayIcon
+  let trayIcon = new Tray(path.join(__dirname, "/img/icon_placeholder.jpg"));
+
+  let trayMenu = Menu.buildFromTemplate([
+    {
+      label: 'Empty Application',
+      enabled: false
+    },
+    {
+      label: 'Settings',
+      click: function () {
+        console.log("Clicked on settings")
+      }
+    },
+    {
+      label: 'Help',
+      click: function () {
+        console.log("Clicked on Help")
+      }
+    }
+  ]);
+  trayIcon.setContextMenu(trayMenu);
+
+  //
   app.on("activate", function () {
     // macOS dock functionality
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
-app.on("window-all-closed", function(){
+app.on("window-all-closed", function() {
   // macOS menu functionality
   if (process.platform !== "darwin") app.quit()
 })
--- app/js/menu.js
+++ app/js/menu.js
@@ -1,21 +1,21 @@
 const { remote } = require("electron");
 var win = remote.BrowserWindow.getFocusedWindow();
 
-$("#minimise").click(function(){
+$("#minimise").click(function() {
     win.minimize();
 });
 
-function updateMaxButton(){
+function updateMaxButton() {
     win.isMaximized() ? $("#maximise span").attr("class", "far fa-window-restore") : $("#maximise span").attr("class", "far fa-window-maximize");
 };
 
-$("#maximise").click(function(){
+$("#maximise").click(function() {
     win.isMaximized() ? win.unmaximize() : win.maximize()
 });
 
 win.on("maximize", updateMaxButton).on("unmaximize", updateMaxButton);
 $(document).ready(updateMaxButton);
 
-$("#close").click(function(){
+$("#close").click(function() {
     win.close();
 });
